93ecc3eede8483cd04bde448205beb633ee67828
kevin5874 <39664646+kevin5874@users.noreply.github.com>
2018-08-02 18:30:28 -0500

    Reading from excel doc, writing to DB

---
+++ ConsoleApp19/Employee.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp19
+{
+    class Employee
+    {
+        public string FName { get; set; }
+        public string LName { get; set; }
+        public string ID { get; set; }
+        public string EmpType { get; set; }
+        public string payType { get; set; }
+        public string Phone { get; set; }
+        public DateTime DOB { get; set; }
+        public Employee (string FName, string LName, string ID, string EmpType, string payType, string Phone, DateTime DOB)
+        {
+            this.FName = FName;
+            this.LName = LName;
+            this.ID = ID;
+            this.EmpType = EmpType;
+            this.payType = payType;
+            this.Phone = Phone;
+            this.DOB = DOB;
+        }
+        public void ToDB()//adding employees to DB
+        {
+            string connectionString = @"Data Source=DESKTOP-1N6P132;Initial Catalog=EmpTest;Integrated Security=True";
+            string queryString = @"INSERT INTO Employees (FName, LName, ID, EmpType, PayType, Phone, DOB) VALUES
+                                      (@FName, @LName, @ID, @EmpType, @PayType, @Phone, @DOB)";
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand(queryString, conn);
+                cmd.Parameters.Add("@FName", SqlDbType.NVarChar);
+                cmd.Parameters.Add("@LName", SqlDbType.NVarChar);
+                cmd.Parameters.Add("@ID", SqlDbType.NVarChar);
+                cmd.Parameters.Add("@EmpType", SqlDbType.NVarChar);
+                cmd.Parameters.Add("@PayType", SqlDbType.NVarChar);
+                cmd.Parameters.Add("@Phone", SqlDbType.NVarChar);
+                cmd.Parameters.Add("@DOB", SqlDbType.Date);
+                cmd.Parameters["@FName"].Value = FName;
+                cmd.Parameters["@LName"].Value = LName;
+                cmd.Parameters["@ID"].Value = ID;
+                cmd.Parameters["@EmpType"].Value = EmpType;
+                cmd.Parameters["@PayType"].Value = payType;
+                cmd.Parameters["@Phone"].Value = Phone;
+                cmd.Parameters["@DOB"].Value = DOB;
+
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                conn.Close();
+            }
+        }
+    }
+}
--- ConsoleApp19/Program.cs
+++ ConsoleApp19/Program.cs
@@ -1,15 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
+using Excel = Microsoft.Office.Interop.Excel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Microsoft.Office.Interop.Excel;
+//So the biggest downside to this style of program that I can forsee is that it's only going to work if the data is formated in excell
+//correctly.
 namespace ConsoleApp19
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Excel.Application app = new Excel.Application();
+            Excel.Workbook wb = app.Workbooks.Open(@"C:\Users\kevin\Downloads\TestBook.xlsx");//place the path to an excel doc here
+            Excel.Worksheet sheet = wb.Sheets[1];
+            Excel.Range range = sheet.UsedRange;
+            List<Employee> emps = new List<Employee>();
+            for(int i = 2; i <= range.Rows.Count; i++)
+            {
+                int j = 1;
+
+                string s = sheet.Cells[i, 1].Value;
+                string a = sheet.Cells[i, j + 1].Value;
+                string b = sheet.Cells[i, j + 2].Value.ToString();
+                string c = sheet.Cells[i, j + 3].Value;
+                string d = sheet.Cells[i, j + 4].Value;
+                string e =sheet.Cells[i, j + 5].Value.ToString();
+                DateTime f =sheet.Cells[i, j + 6].Value;
+
+                    emps.Add(new Employee(s,a,b,c,d,e,f));
+               
+                
+            }
+            foreach(Employee emp in emps)//loops through any employees in the list
+            {
+                emp.ToDB();//calls a method to add them to the DB
+            }
+
         }
     }
 }
